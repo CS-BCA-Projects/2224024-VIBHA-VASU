11efaf2e95a00ea8771472de3bd34d5312e51473
Vibahvasu Bhadauria <vibhavasudsvv2003@gmail.com>
2025-02-05 15:54:07 +0530

    version 01 [(03:54 PM)(05/02/2025)] 'Project setup'

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,35 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs";
+
+// (async function() {
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDNARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDNARY_API_KEY, 
+        api_secret: process.env.CLOUDNARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+    });
+
+    const uploadOnCloudinary=async (localFilePath)=>{
+        try {
+            if (!localFilePath) {
+                return null;
+            } 
+            else {
+                const uploadresult=await cloudinary.uploader.upload(
+                    localFilePath,{
+                        resource_type:'auto'
+                    }
+                )
+                fs.unlinkSync(localFilePath);//to delete file from server
+               // console.log("File has uploaded ",uploadresult.url);
+                return uploadresult
+            }
+        } catch (error) {
+            fs.unlinkSync(localFilePath);//to delete file from server
+            return null;
+        }
+    }
+
+
+    export {uploadOnCloudinary}
